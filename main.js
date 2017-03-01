bb60c1c169de04a8f3cc82327702ad5d35e736cf
lcxmars <lcxmars@gmail.com>
2017-03-01 13:19:00 -0300

    Primeras pruebas

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 const electron = require('electron');
+const path = require('path');
+const url = require('url');
 
-const BrowserWindow = electron.BrowserWindow;
 const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
 let win;
 
-app.on('ready', function () {
-  win = new BrowserWindow({width: 800, height: 600, frame: true});
-  win.show();
+function createWindow () {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: true
+  });
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  win.webContents.openDevTools();
+  win.on('closed', () => win = null);
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (win === null) createWindow();
 });
